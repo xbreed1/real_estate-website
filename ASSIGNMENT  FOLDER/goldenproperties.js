56366e645a66b5502cca1adbf67ffd645af3eeba
xbreed1 <xpecialbreed@gmail.com>
2022-10-18 00:39:00 +0100

    Assignment folder containing real estate website design, using html,css,bootstrap and javascript

---
+++ ASSIGNMENT  FOLDER/goldenproperties.js
@@ -0,0 +1,8 @@
+const contactName = document.querySelector('#contactName');
+const email = document.querySelector('#email');
+const number = document.querySelector('#number') 
+const mb = document.querySelector('#submit')        
+
+Form.addEventListener('submit', function (e) {
+    e.preventDefault();
+});
